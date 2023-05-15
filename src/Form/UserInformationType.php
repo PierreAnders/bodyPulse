c7d486f858a52843b41f1f3038ba3749f69ba6b2
PierreAnders <pierre.untas@gmail.com>
2023-05-15 10:19:42 +0200

    update user info new

--- src/Form/UserInformationType.php
+++ src/Form/UserInformationType.php
@@ -4,8 +4,11 @@ namespace App\Form;
 
 use App\Entity\UserInformation;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 
 class UserInformationType extends AbstractType
 {
@@ -14,10 +17,22 @@ class UserInformationType extends AbstractType
         $builder
             ->add('firstName')
             ->add('lastName')
-            ->add('gender')
-            ->add('dateOfBirth')
-            ->add('height')
-            ->add('weight')
+            ->add('gender', ChoiceType::class, [
+                'choices' => [
+                    'Male' => 'male',
+                    'Female' => 'female',
+                ],
+                'placeholder' => 'Gender',
+            ])           
+            ->add('dateOfBirth', BirthdayType::class, [
+                'years' => range(1903, date('Y')),
+            ])
+            ->add('height', NumberType::class, [
+                'scale' => 2,
+            ])
+            ->add('weight', NumberType::class, [
+                'scale' => 2,
+            ])
         ;
     }
 
